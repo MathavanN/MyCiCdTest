17c4b4829e3a130e361ee0d528e147df7eb6e610
math2018avan <math2018avan@gmail.com>
2019-03-23 15:16:07 +0800

    console application added

--- MyConsoleApp/MyConsoleApp/MyProgram.cs
+++ MyConsoleApp/MyConsoleApp/MyProgram.cs
@@ -6,4 +6,13 @@ namespace MyConsoleApp
     {
         public string Get(int left, int right) => $"{left + right}";
     }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var result = new MyProgram().Get(5, 5);
+            Console.WriteLine(result);
+        }
+    }
 }
