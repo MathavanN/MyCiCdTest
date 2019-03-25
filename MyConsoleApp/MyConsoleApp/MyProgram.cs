b0c19192ebcae0706d8f797644e74b3c0efcfec9
math2018avan <math2018avan@gmail.com>
2019-03-25 22:53:40 +0800

    Update MyProgram.cs
    user nedd to enter the values

--- MyConsoleApp/MyConsoleApp/MyProgram.cs
+++ MyConsoleApp/MyConsoleApp/MyProgram.cs
@@ -4,17 +4,19 @@ namespace MyConsoleApp
 {
     public class MyProgram
     {
-        public string Get(int left, int right) => $"{left + right}";
+        public int Add(int a, int b) => a + b;
     }
 
     public class Program
     {
         public static void Main()
         {
-            var a = 6;
-            var b = 6;
-            var result = new MyProgram().Get(a, b);
-            Console.WriteLine($"Result of Get({a}, {b}) is {result}");
+            Console.WriteLine("Enter the first number:");
+            var a = Convert.ToInt32(Console.ReadLine().ToString());
+            Console.WriteLine("Enter the second number:");
+            var b = Convert.ToInt32(Console.ReadLine().ToString());
+            var result = new MyProgram().Add(a, b);
+            Console.WriteLine($"Result of Add({a}, {b}) is {result}");
         }
     }
 }
