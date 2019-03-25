7c643c1637da1a47910bc1d750ba482580487041
math2018avan <math2018avan@gmail.com>
2019-03-25 23:06:31 +0800

    Update MyProgramTest.cs
    more test cases added
    proper name changed

--- MyConsoleApp/MyConsoleApp.Test/MyProgramTest.cs
+++ MyConsoleApp/MyConsoleApp.Test/MyProgramTest.cs
@@ -5,13 +5,23 @@ namespace MyConsoleApp.Test
     public class MyProgramTest
     {
         [Fact]
-        public void VerifyGet()
+        public void Adding_4_And_3_Should_Return_7()
         {
-            const int firstNumber = 19;
-            const int secondNumber = 23;
+            const int firstNumber = 4;
+            const int secondNumber = 3;
             var objTargetClass = new MyProgram();
             var result = objTargetClass.Add(firstNumber, secondNumber);
-            Assert.Equal((firstNumber + secondNumber), result);
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void Adding_Negative_10_And_3_Should_Return_7()
+        {
+            const int firstNumber = -10;
+            const int secondNumber = 3;
+            var objTargetClass = new MyProgram();
+            var result = objTargetClass.Add(firstNumber, secondNumber);
+            Assert.Equal(-7, result);
         }
     }
 }
