058df4aed4ba03d239ff7799a59789208eebfe8e
math2018avan <48410577+math2018avan@users.noreply.github.com>
2019-03-26 22:03:00 +0800

    Add function second param changed
    Add function second param changed

--- MyConsoleApp/MyConsoleApp/MyProgram.cs
+++ MyConsoleApp/MyConsoleApp/MyProgram.cs
@@ -4,7 +4,7 @@ namespace MyConsoleApp
 {
     public class MyProgram
     {
-        public int Add(int aa, int b) => aa + b;
+        public int Add(int aa, int bb) => aa + bb;
     }
 
     public class Program
